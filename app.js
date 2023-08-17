e5825c02870aa8ca8bdec6c9693b7549b369f7e4
im-usb <udaysbaghel@gmail.com>
2023-08-17 23:42:32 +0530

    Upload Meme-Maker Project

--- app.js
+++ app.js
@@ -9,11 +9,18 @@ const colorPicker = document.querySelector("#color");
 const modeBtn = document.querySelector("#fill-color");
 const resetBtn = document.querySelector("#reset");
 const eraseBtn = document.querySelector("#erase");
+const fileInput = document.querySelector("#file");
+const textInput = document.querySelector("#text");
+const saveBtn = document.querySelector("#save");
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 500;
 
 // Set canvas dimensions and initial line width
 canvas.width = 800;
 canvas.height = 500;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 
 // Initialize painting-related variables
 let isPainting = false;
@@ -35,7 +42,7 @@ function onMouseDown(event) {
   if (isFill) {
     // Fill canvas with selected color if in fill mode
     ctx.fillStyle = colorPicker.value;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     isPainting = false;
   } else {
     isPainting = true; // Start painting if not in fill mode
@@ -81,7 +88,7 @@ function onModeBtnClick() {
 // Function to reset the canvas to a blank state
 function onResetBtnClick() {
   ctx.fillStyle = "#ffffff";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   ctx.beginPath();
 }
 
@@ -96,14 +103,49 @@ function onEraseBtnClick() {
   }
 }
 
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.beginPath;
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  };
+}
+
+function onDoubleClick(event) {
+  ctx.save();
+  const text = textInput.value;
+  // const currLineWidth = lineWidth.value;
+  ctx.beginPath;
+  ctx.lineWidth = 1;
+  ctx.font = "48px sans-serif";
+  ctx.fillText(text, event.offsetX, event.offsetY);
+  // ctx.lineWidth = currLineWidth;
+  ctx.restore();
+}
+
+function onSaveBtnClick() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myDrawing.png";
+  a.click();
+}
+
 // Event listeners for various interactions on the canvas
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 canvas.addEventListener("mouseleave", onMouseUp); // Handle mouse leaving the canvas while painting
+canvas.addEventListener("dblclick", onDoubleClick);
 lineWidth.addEventListener("change", onLineWidthUpdate);
 colorPicker.addEventListener("change", onColorSelect);
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick)); // Set up color selection events
 modeBtn.addEventListener("click", onModeBtnClick);
 resetBtn.addEventListener("click", onResetBtnClick);
 eraseBtn.addEventListener("click", onEraseBtnClick);
+saveBtn.addEventListener("click", onSaveBtnClick);
+fileInput.addEventListener("change", onFileChange);
